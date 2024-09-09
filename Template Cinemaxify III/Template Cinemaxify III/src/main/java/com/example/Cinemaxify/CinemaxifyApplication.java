3ebdc9a779948191910f6155b867ce7ac6d4d4e8
Aditya Srivastava <154252827+Aditya9779@users.noreply.github.com>
2024-09-09 17:24:12 +0530

    Add files via upload

--- Template Cinemaxify III/Template Cinemaxify III/src/main/java/com/example/Cinemaxify/CinemaxifyApplication.java
+++ Template Cinemaxify III/Template Cinemaxify III/src/main/java/com/example/Cinemaxify/CinemaxifyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.util.Scanner;
 
 @SpringBootApplication
+@Author(name = "Aditya Srivastava" ,date = "04-08-2024")
 public class CinemaxifyApplication {
 
 	public static void main(String[] args) {
