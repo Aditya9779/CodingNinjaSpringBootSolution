255385cd942fc3a9aa096d13e9b980ce7949be07
Aditya Srivastava <154252827+Aditya9779@users.noreply.github.com>
2024-10-05 22:47:05 +0530

    Add files via upload

--- TuneIn Template/TuneIn Template/src/main/java/com/example/TuneIn/TuneInApplication.java
+++ TuneIn Template/TuneIn Template/src/main/java/com/example/TuneIn/TuneInApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.util.Scanner;
 
 @SpringBootApplication
+@Author(name = "Aditya Srivastava", date = "11-08-2024")
 public class TuneInApplication {
 
     public static void main(String[] args) {
