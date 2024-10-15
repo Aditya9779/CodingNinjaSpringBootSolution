4ce0729408d984f7c0c5d097916b8a19d4cfa390
Aditya Srivastava <154252827+Aditya9779@users.noreply.github.com>
2024-10-15 21:02:08 +0530

    Add files via upload

---
+++ Hotel Application JWT(Implementation_correct)/src/main/java/com/cn/hotel/dto/UserRequest.java
@@ -0,0 +1,15 @@
+package com.cn.hotel.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+
+	private String username;
+	private String password;
+
+}
